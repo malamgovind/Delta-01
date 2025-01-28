5f365f8c003ca32fc8989a1c744c5a22d1418479
malam govind <malamgovindd1@gmail.com>
2025-01-28 20:44:44 +0530

    ok

---
+++ Day-00/web01/index.js
@@ -0,0 +1,14 @@
+function checkCheckboxes(currentCheckbox) {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const checkedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked);
+
+    
+    if (checkedCheckboxes.length > 2) {
+        for (const checkbox of checkedCheckboxes) {
+            if (checkbox !== currentCheckbox) {
+                checkbox.checked = false;
+                break; 
+            }
+        }
+    }
+}
